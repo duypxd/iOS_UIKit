a6dc82e72bcd6ac5c01c20abd9eb506091d319c9
Duy Pham <duypham@DuyPham-MO.local>
2023-07-28 23:58:57 +0700

    first navigation

--- Gespage/ViewController.swift
+++ Gespage/ViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var onPressNextPage: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func onPressNextPage(_ sender: UIButton) {
+        let storyboard = UIStoryboard(name: "OnboardStoryboard", bundle: nil)
+        if let welcomeViewController = storyboard.instantiateViewController(withIdentifier: "OnboardViewController") as? OnboardViewController {
+            self.navigationController?.pushViewController(welcomeViewController, animated: true)
+        }
+    }
 }
 
---
+++ Gespage/ViewControllers/Onboard/OnboardViewController.swift
@@ -0,0 +1,16 @@
+//
+//  OnboardViewController.swift
+//  Gespage
+//
+//  Created by Duy Pham on 28/07/2023.
+//
+
+import UIKit
+
+class OnboardViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
