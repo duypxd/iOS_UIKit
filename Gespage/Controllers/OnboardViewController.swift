d591929f520a690e9d7f1aa6109e6bc91683bc3b
Duy Pham <duypham@DuyPham-MO.local>
2023-07-29 00:33:10 +0700

    refactor strucuture

--- Gespage/Controllers/OnboardViewController.swift
+++ Gespage/Controllers/OnboardViewController.swift
@@ -9,8 +9,16 @@ import UIKit
 
 class OnboardViewController: UIViewController {
 
+    @IBOutlet weak var onSkipOnboard: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
+    @IBAction func onSkipOnboard(_ sender: Any) {
+        let storyboard = UIStoryboard(name: "WelcomeStoryboard", bundle: nil)
+        if let welcomeViewController = storyboard.instantiateViewController(withIdentifier: "WelcomeViewController") as? WelcomeViewController {
+            self.navigationController?.pushViewController(welcomeViewController, animated: true)
+        }
+    }
 }
---
+++ Gespage/Controllers/WelcomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  WelcomeViewController.swift
+//  Gespage
+//
+//  Created by Duy Pham on 29/07/2023.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .white
+        title = "Welcome Page"
+    }
+    
+}
