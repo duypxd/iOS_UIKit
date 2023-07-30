2186ebf173f69bd412d8e4905e59a5a668ba718f
Duy Pham <duypham@DuyPham-MO.local>
2023-07-30 11:32:20 +0700

    UI: WelcomePage

--- Gespage/Controllers/WelcomeViewController.swift
+++ Gespage/Controllers/WelcomeViewController.swift
@@ -9,11 +9,14 @@ import UIKit
 
 class WelcomeViewController: UIViewController {
     
+    @IBOutlet weak var continueGuestButton: UIButton!
+    @IBOutlet weak var loginButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.backgroundColor = .white
-        title = "Welcome Page"
+        loginButton.layer.cornerRadius = 12.0
+        loginButton.clipsToBounds = true
     }
     
 }
